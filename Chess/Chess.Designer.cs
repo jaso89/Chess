43bdda182e60722b29bb174f986dbc1ac429d8bb
jaso89 <soronen.jani@gmail.com>
2022-03-18 15:38:41 +0200

    Add project files.

---
+++ Chess/Chess.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace Chess
+{
+    partial class Chess
+    {
+        /// Required designer variable.
+        private System.ComponentModel.IContainer components = null;
+
+        /// Clean up any resources being used.
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Chess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 560);
+            this.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.Name = "Chess";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            //this.Load += new System.EventHandler(this.Chess_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
